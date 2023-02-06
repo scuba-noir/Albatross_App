753d9229136493e8fb326f033c05e439d8583df0
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-05 21:38:50 -0500

    secret key

--- albatross_site/settings.py
+++ albatross_site/settings.py
@@ -21,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xf6kh0s*0kxog4!=*srsd&@(alz7003ogq^t87+pj-9e#ohdt9'
-
+#SECRET_KEY = 'django-insecure-xf6kh0s*0kxog4!=*srsd&@(alz7003ogq^t87+pj-9e#ohdt9'
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+    
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
