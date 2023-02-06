1402ff9990c1a9d7fce93f87253ecb31ac0e12ac
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-06 03:47:26 -0500

    Update settings.py

--- albatross_site/settings.py
+++ albatross_site/settings.py
@@ -74,14 +74,10 @@ WSGI_APPLICATION = 'albatross_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES ={
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demo',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'admin',
-        'PASSWORD': 'admin'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
