6bf404b1c560fef07bbd9f3893b51434083a1b70
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-03-29 00:21:45 -0400

    Update settings.py

--- albatross_site/settings.py
+++ albatross_site/settings.py
@@ -129,7 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-print(env('EMAIL_HOST_USER'))
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
